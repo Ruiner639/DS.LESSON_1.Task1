be550e7bf1bf007b522c4af21c72ee73ae95d554
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-30 12:44:39 +0200

    Добавьте файлы проекта.

---
+++ DS.LESSON_1.Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DS.LESSON_1.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Name;
+            string[] Date;
+            Name = new string[1];
+            Name[0] = "John Smith";
+            Date = new string[3];
+            Date[0] = "begin";
+            Date[1] = "mid";
+            Date[2] = "end";
+            Console.WriteLine("Name index is automatically selected 0");
+            int idName = 0;
+            Console.WriteLine("Enter date of birth index(0,1,2)");
+            //John Smith’s birthday is in beginning [mid, end] of March
+            int idBirth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Name[idName] + "’s birthday is in " + Date[idBirth] + " of March");
+        }
+    }
+}
